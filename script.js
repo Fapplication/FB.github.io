9a283ea916e28fcdd27c4e2c22100490906ea4e6
Fapplication <41576931+Fapplication@users.noreply.github.com>
2026-05-20 01:35:43 -0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("FBGC Website Loaded Successfully");
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+// Smooth Scroll Animation
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const top = window.scrollY;
+        const offset = section.offsetTop - 200;
+        const height = section.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            section.classList.add("show");
+        }
+
+    });
+
+});
